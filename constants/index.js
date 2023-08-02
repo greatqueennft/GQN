693eae23e21ff1ba66c7aaedda4ed5e073472fa5
greatqueennft <141251504+greatqueennft@users.noreply.github.com>
2023-08-02 21:14:13 +0530

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x2a91679c66cdafee3e6e8a36fb6fbe8933861a2f";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
